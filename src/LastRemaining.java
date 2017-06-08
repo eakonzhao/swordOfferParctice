4e0965f0fe835fa99a033f95c5baceb30ea1c65a
EakonZhao <eakonzhao@gmail.com>
2017-06-08 20:53:21 +0800

    getOfferCodePractice

---
+++ src/LastRemaining.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Eakon on 2017/6/8.
+ *
+ * 圆圈中最后剩下的数字
+ *
+ * 题目描述：0，1，...,n-1 这n个数字排成一个圆圈，从数字0开始每次
+ * 从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ */
+public class LastRemaining {
+    public int LastRemaining_Solution(int n, int m){
+         
+    }
+}
